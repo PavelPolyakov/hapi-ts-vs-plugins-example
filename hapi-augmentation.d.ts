c3304a150ef4cd7a9579cf6cc92533a88b741ba9
Mohamed Hegazy <mhegazy@microsoft.com>
2017-10-09 10:48:27 -0700

    Augment hapi::Server

---
+++ hapi-augmentation.d.ts
@@ -0,0 +1,14 @@
+import * as DTHapi from "hapi";
+
+declare module "hapi" {
+    export interface Server {
+        some_server_method(arg1: number): string;
+        nono(wow: string, liam: boolean): boolean;
+    }
+
+    export interface PluginsStates {
+        db: {
+            wow: boolean;
+        };
+    }
+}
